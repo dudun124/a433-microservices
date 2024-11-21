376f7000beb2cb24058837e8894053def5ef3306
dudun124 <hamdanriza28@gmail.com>
2024-11-22 01:56:17 +0700

    submission requirements

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,7 +7,7 @@ docker build -t item-app:v1 .
 docker tag item-app:v1 ghcr.io/dudun124/item-app:v1
 
 # Login ke GitHub Container Registry
-export CR_PAT=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
+
 echo $CR_PAT | docker login ghcr.io -u dudun124 --password-stdin
 
 # Push Docker image ke GitHub Container Registry
