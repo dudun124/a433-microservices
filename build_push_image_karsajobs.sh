a7d13e4a268fa031c5a7ab9c2e9f40f340800ed5
dudun124 <hamdanriza28@gmail.com>
2024-11-25 02:34:55 +0700

    update

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -7,7 +7,7 @@ docker build -t karsajobs:latest .
 docker tag karsajobs:latest ghcr.io/dudun124/karsajobs:latest
 
 # Login ke GitHub Container Registry
-export CR_PAT=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
+
 echo $CR_PAT | docker login ghcr.io -u dudun124 --password-stdin
 
 # Push Docker image ke GitHub Container Registry
